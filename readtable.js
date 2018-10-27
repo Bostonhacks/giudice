cf595e159ec4ab7719c251e95a51a94ff52aa881
Warren-Partridge <maps@perdix.org>
2018-10-27 15:14:16 -0400

    Assign table numbers to projects

--- readtable.js
+++ readtable.js
@@ -2,7 +2,15 @@ const csvFilePath='./submissions.csv'
 const csv=require('csvtojson')
 csv()
 .fromFile(csvFilePath)
-.then((jsonObj)=>{
+.then((jsonObj) => {
+	let tableAssignmentIterator = 0;
+
+    for (var i in jsonObj) { // Iterate through all projects and assign table
+    	var project = jsonObj[i]
+    	project.table = tableAssignmentIterator;
+		tableAssignmentIterator++;
+    }
+
     console.log(jsonObj);
 }).then(() => {
 	const jsonArray = csv().fromFile(csvFilePath);
