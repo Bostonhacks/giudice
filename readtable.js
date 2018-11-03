72712a4feee56784512b0fb1a6b2c0a2ec5d94ca
Warren-Partridge <maps@perdix.org>
2018-11-02 22:32:08 -0400

    Map tables to each judging track
    Co-authored-by: Sarah Rieger <srieger@bu.edu>

--- readtable.js
+++ readtable.js
@@ -1,17 +1,48 @@
 const csvFilePath='./submissions.csv'
 const csv=require('csvtojson')
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj) => {
-	let tableAssignmentIterator = 0;
 
-    for (var i in jsonObj) { // Iterate through all projects and assign table
-    	var project = jsonObj[i]
+
+
+function assignTables(projectsJson) {
+
+	let tableAssignmentIterator = 1;
+
+    for (var i in projectsJson) { // Iterate through all projects and assign tables
+    	var project = projectsJson[i]
     	project.table = tableAssignmentIterator;
 		tableAssignmentIterator++;
     }
 
-    console.log(jsonObj);
-}).then(() => {
-	const jsonArray = csv().fromFile(csvFilePath);
+    //console.log(projectsJson);
+}
+
+
+
+function getListOfTracks(projectsJson) {
+  trackMap = {} // K-V: K=track, V=array of judging tables in that track
+  trackMap['General Prize'] = [] // This will contain every table (every project is considered for general prize)
+
+  for (var project of projectsJson) { // Same as for..each but javascript
+    tracks = project['Desired Prizes'].split(', '); // Make array of strings of prizes per team
+    for (let i=0; i<tracks.length; i++) {
+      if (!(tracks[i] in trackMap)) { // if track already exists in dictionary
+        trackMap[tracks[i]] = [];
+        //console.log(trackMap);
+      }
+
+      trackMap[tracks[i]].push(project['table']); // Appends table to list of tables signed up for that track
+    }
+
+    trackMap['General Prize'].push(project['table']); // Append table to list of tables signed up for general prize
+  }
+  //console.log(trackMap);
+
+  return trackMap;
+}
+
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj) => {
+  assignTables(jsonObj);
+  getListOfTracks(jsonObj);
 })
