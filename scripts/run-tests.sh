c9fe72c4327198f81caf28498f16029023c87641
jli12 <jasonfli12@gmail.com>
2019-02-22 12:11:14 -0500

    judges only have one track now

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python -c 'from tests.main_tests import *; print run_tests()'
-python -c 'from tests.projects_tests import *; print run_tests()'
+python3 -c 'from tests.main_tests import *; print run_tests()'
+python3 -c 'from tests.projects_tests import *; print run_tests()'
